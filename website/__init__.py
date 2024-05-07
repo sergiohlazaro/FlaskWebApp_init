2618c90f7260d0d4814d630b90a4e78694c55ae3
sergiohlazaro <sergio.hlazaro@alumnos.upm.es>
2024-05-07 17:57:09 +0200

    Configuración inicial para la fase 2 en el diseño de las plantillas html finalizada, a continuación se comienza con los formularios.

--- website/__init__.py
+++ website/__init__.py
@@ -8,6 +8,8 @@ def create_app():
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/auth/') # This is the URL prefix for the auth blueprint, for example: /auth/login
+    # This is the URL prefix for the auth blueprint, for example: /auth/login
+    # app.register_blueprint(auth, url_prefix='/auth') 
+    app.register_blueprint(auth, url_prefix='/') 
 
     return app
